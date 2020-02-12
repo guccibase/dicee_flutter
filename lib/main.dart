dfafe479e6672271cc331eda7c3350a3691120de
guccibase <tahirnasri96@gmail.com>
2020-02-12 12:03:09 -0500

    dice game

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,78 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  Random random = new Random();
+
+  int rightDiceNumber = 1;
+  int leftDiceNumber = 2;
+  String screenText = 'click';
+
+  diceChange() {
+    rightDiceNumber = random.nextInt(6) + 1;
+    leftDiceNumber = random.nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        diceChange();
+                        leftDiceNumber == rightDiceNumber
+                            ? screenText =
+                                'Congrats! it\'s a match $leftDiceNumber:$rightDiceNumber'
+                            : screenText = ' Click ';
+                      });
+                    },
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        diceChange();
+                        leftDiceNumber == rightDiceNumber
+                            ? screenText =
+                                'Congrats! it\'s a match $leftDiceNumber:$rightDiceNumber'
+                            : screenText = ' Click ';
+                      });
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 120,
+          ),
+          Container(
+            height: 60,
+            width: 300,
+            child: Center(
+              child: Text(
+                screenText,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
